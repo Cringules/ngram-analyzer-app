5c9516cc0efe1992d8b2812f7c55ea5f986b1df9
MilkyCode13 <shmayhel.andrey@gmail.com>
2023-04-30 16:58:01 +0300

    Added basic integration of Lib

--- Cringules.NGram.App/Model/WorkSession.cs
+++ Cringules.NGram.App/Model/WorkSession.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -12,13 +13,17 @@ public partial class WorkSession : ObservableObject
 {
     [ObservableProperty] private PlotData _data = null!;
 
-    [ObservableProperty] private List<Peak> _peaks = new();
-
     [ObservableProperty] private double _waveLength;
 
     [ObservableProperty] private bool _determineNoiseLevel;
 
     [ObservableProperty] private int _noiseLevel;
+
+    [ObservableProperty] private List<Point> _peakBoundaries = new();
+
+    [ObservableProperty] private List<XrayPeak> _xrayPeaks = new();
+
+    [ObservableProperty] private List<Peak> _peaks = new();
 
     [JsonIgnore] public DiffractogramPlotModel Model { get; } = new();
 
@@ -34,18 +39,27 @@ public partial class WorkSession : ObservableObject
     }
 
     [RelayCommand]
-    private void CalculateDataPeaks()
-    {
-    }
-
-    [RelayCommand]
     private void StartAnalysis()
     {
+        var xray = new Xray(Data.Points.Select(point => new Point(point.Angle, point.Intensity)));
+        PeakBoundaries = xray.GetPeakBoundaries();
+
+        XrayPeaks.Clear();
+        for (var i = 0; i < PeakBoundaries.Count - 1; i++)
+        {
+            XrayPeaks.Add(xray.GetPeak(PeakBoundaries[i].X, PeakBoundaries[i + 1].X));
+        }
+
+        Peaks = XrayPeaks.Select(peak =>
+        {
+            var analyzer = new XrayPeakAnalyzer(peak);
+            return new Peak(analyzer.GetTopAngle(), 0, analyzer.GetIntensityMax(), analyzer.GetIntensityIntegral());
+        }).ToList();
     }
 
     partial void OnDataChanged(PlotData? value)
     {
         Model.Update(value?.Points);
-        Peaks = PeakFinder.FindPeaks(value);
+        // Peaks = PeakFinder.FindPeaks(value);
     }
 }
