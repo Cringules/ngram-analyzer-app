248bffde799cc13ef2b49ef0f0f4b8a6fcadac86
eachekalina <eachekalina@edu.hse.ru>
2023-05-05 00:23:39 +0300

    Update UI a bit

---
+++ Cringules.NGram.App/Model/NamedItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Cringules.NGram.App.Model;
+
+public class NamedItem<T>
+{
+    public string Name { get; }
+    public T Value { get; }
+
+    public NamedItem(string name, T value)
+    {
+        Name = name;
+        Value = value;
+    }
+}
