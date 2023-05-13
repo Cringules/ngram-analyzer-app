0cc0a562caeb002290417e7eb76dc18df7ddd64f
MilkyCode13 <shmayhel.andrey@gmail.com>
2023-05-13 09:54:55 +0300

    Add Excel export

---
+++ Cringules.NGram.App/Export/ExcelResultExporter.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using ClosedXML.Report;
+using Cringules.NGram.App.Model;
+
+namespace Cringules.NGram.App.Export;
+
+public class ExcelResultExporter : IResultExporter
+{
+    public string FileExtension => ".xlsx";
+    public void Export(WorkSession session, string filename)
+    {
+        Stream? stream = typeof(ExcelResultExporter).Assembly.GetManifestResourceStream(
+            "Cringules.NGram.App.Resources.report_template.xlsx");
+        var template = new XLTemplate(stream);
+        
+        template.AddVariable(session);
+        template.AddVariable(@"DataPoints", session.Data.Points);
+        template.AddVariable(@"SmoothedDataPoints", session.SmoothedData?.Points);
+        template.Generate();
+        template.SaveAs(filename);
+    }
+}
